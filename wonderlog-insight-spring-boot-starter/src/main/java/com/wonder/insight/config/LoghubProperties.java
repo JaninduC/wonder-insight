e197362624912e18688eed7d063f1d7dbf09dc82
JaninduC <janindurox077@gmail.com>
2026-01-01 14:38:31 +0530

    Add files via upload

---
+++ wonderlog-insight-spring-boot-starter/src/main/java/com/wonder/insight/config/LoghubProperties.java
@@ -0,0 +1,41 @@
+package com.wonder.insight.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "wonder-insight")
+@Data
+public class LoghubProperties {
+
+
+    private String url;
+    private String token;
+    private boolean enabled;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+
+
+}
---
+++ wonderlog-insight-spring-boot-starter/src/main/java/com/wonder/insight/exception/TrackErrorBypassException.java
@@ -0,0 +1,8 @@
+package com.wonder.insight.exception;
+
+public class TrackErrorBypassException extends RuntimeException {
+    public TrackErrorBypassException(Throwable cause) {
+        super(cause);
+    }
+
+}
